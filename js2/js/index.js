2a55143c02a4043d81d4fa32d024fcf916233ad3
Anton <141570541+Anonymusm@users.noreply.github.com>
2024-12-16 16:50:39 +0100

    finished hw

---
+++ js2/js/index.js
@@ -0,0 +1,22 @@
+// 1
+let result = 5 + 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+// 2
+let email = "standoffantoshka@gmail.com";
+if (email.includes("@")) {
+  console.log(email.length);
+}
+
+// 3
+let name1 = "My";
+let name2 = "name";
+let name3 = "is";
+let fullName = `${name1} ${name2} ${name3} Viktor`;
+console.log(fullName)
+
+// 4
+let userName = 'Олександра';
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
